37532e1ea9488891fa5656a7c482f2dd1683393d
Demon <374405702@qq.com>
2017-12-23 15:50:54 +0800

    Update STMP_DEMO.py

--- STMP_DEMO.py
+++ STMP_DEMO.py
@@ -1,11 +1,15 @@
 # encoding:utf-8
 import yagmail
 
-yag = yagmail.SMTP(user='374405702@qq.com', password='gaqpyccovwfqcabf', host='smtp.qq.com')
-send_contents = 'this is a test.l just use the yagmail lib to send you a mail'
-yag.send('1799803903@qq.com', '良心库，yagmail', send_contents)
+yag = yagmail.SMTP(user='5201314@qq.com', password='xxxxxxxxxxx', host='smtp.qq.com')
+send_contents = 'this is a test.l just use the yagmail lib to send a mail'
+yag.send('5201315@qq.com', '良心库，yagmail', send_contents)
 
+"""
+password='xxxxxxxxxxx'中的xxxxxxxxxxx为QQ邮箱登录授权码。
+不是明码。
 
+"""
 
 
 
